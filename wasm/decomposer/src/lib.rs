39b953c07a0c0197008a728e8997d93596865402
livewing.net <dev@livewing.net>
2022-10-08 21:54:01 +0900

    Update wasm dependencies

--- wasm/decomposer/src/lib.rs
+++ wasm/decomposer/src/lib.rs
@@ -9,5 +9,5 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 pub fn decompose_from_counts(counts: &[u8], meld: i32) -> JsValue {
     let results = decompose_min_shanten(counts, meld);
-    JsValue::from_serde(&results).unwrap_or(JsValue::NULL)
+    serde_wasm_bindgen::to_value(&results).unwrap_or(JsValue::NULL)
 }
